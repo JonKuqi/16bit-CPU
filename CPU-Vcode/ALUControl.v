2870a7c97c0bc1b635d5b290631861c05d3d357a
Jon Kuçi <jon.kuci@student.uni-pr.edu>
2024-01-03 00:50:40 +0100

    ALUControl

--- CPU-Vcode/ALUControl.v
+++ CPU-Vcode/ALUControl.v
@@ -1,36 +1,60 @@
 `timescale 1ns / 1ps
 
 
+//Mundet me pase gabime, logjika besoj qe eshte e duhur
+
 //ALU Control (CU_OUT_x, Function Code nga R-formati, Opcode, T19) - eshte shtuar ALUOp per I-format qe nuk eshte ne foto po kerkohet ne detyre
 module ALUControl(
+
+
 input [1:0] ALUOp,
-input [5:0] Funct,
-input [5:0] opcode,
+input [1:0] Funct,
+input [4:0] opcode,
 output reg [3:0] ALUCtrl
 );
 
 always @(ALUOp)
 begin
 case(ALUOp) // Pyet per vleren e ALUOp, 00-lw,sw; 01-beq,bne, 10-R-format, 11 - I-format
-    2'b00: ALUCtrl = 4'b0010; //LW+SW (mbledhje)
-    2'b01: ALUCtrl = 4'b0110; //BEQ+BNE (zbritje)
-    2'b10: //R-Format
-        case(Funct)
-            6'b100000: ALUCtrl = 4'b0010; //ADD
-            6'b100010: ALUCtrl = 4'b0110; //SUB
-            6'b100100: ALUCtrl =4'b0000; //AND
-            6'd37 : ALUCtrl = 4'd1; //OR
-            6'd42 : ALUCtrl = 4'd7; //SLT
+    2'b00: ALUCtrl = 4'b0100; //LW+SW (mbledhje)
+    2'b01: ALUCtrl = 4'b1100; //BEQ+BNE (zbritje)
+    2'b10: //R-Format sipas FUNCT
+	 case(opcode) //R-Format
+          4'b0XXX:	 
+		  case(opcode)
+		  4'b0000:
+           case(Funct)
+            2'b00: ALUCtrl = 4'b0000; //AND
+		    2'b01: ALUCtrl = 4'b0010; //OR
+			2'b10: ALUCtrl = 4'b0011; //XOR
+			endcase
+		   4'b0001:
+			 case(Funct)
+            2'b00: ALUCtrl = 4'b0100; //ADD
+			2'b01: ALUCtrl = 4'b1100; //SUB
+			endcase
+		   4'b0010:
+		    case(Funct)
+            2'b00: ALUCtrl = 4'b0110; //SLL
+			2'b01: ALUCtrl = 4'b0111; //SRA
+			endcase
+	
         endcase
-
-    2'b11: //I-format
-        case(opcode)
-            6'b000100: ALUCtrl = 4'b0010; // per ADDI
-            6'b000110: ALUCtrl = 4'b0000; // per ANDI
-        endcase
-endcase
+	2'b11:  //Ne baze te OPCODEVE I-format	
+	 case(opcode)
+          4'b1XXX:	
+       case(opcode)
+		4'b1001:
+			 ALUCtrl = 4'b0101; //ADDI
+		4'b1010:
+			 ALUCtrl = 4'b1101; //SUBI
+		4'b1011:
+			 ALUCtrl = 4'b0001; //SLTI
+		endcase
+		
+      endcase
+	  	
+ endcase
 end
-
-
 
 endmodule
